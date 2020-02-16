28e61b733006a7fc94d8ccf6a61ad4cac8a857c7
Asylturatbek <asylturatbek@gmail.com>
2020-02-16 12:11:16 +0600

    methods of arrays

---
+++ prep/arraymethods.js
@@ -0,0 +1,44 @@
+const people = [
+	{ name: 'Vladilen', age: 24, budget: 300 },
+	{ name: 'Max', age: 27, budget: 400 },
+	{ name: 'Alan', age: 19, budget: 1200 },
+	{ name: 'Sagyn', age: 30, budget: 1400 },
+	{ name: 'Suvorov', age: 45, budget: 3000 }
+] 
+
+
+// forEach
+// people.forEach(function(person, index, pArr) {
+// 	console.log(person)
+// 	console.log(index)
+// })
+
+// Map
+// const newPeople = people.map(person => {
+// 	return `${person.name} ${person.age}`
+// })
+// console.log(newPeople)
+
+// Filter
+ // const adults = people.filter(function (people) {
+ // 	return people.age>25
+ // })
+ // console.log(adults)
+
+// Reduce
+const sum = people.reduce((total, person) => {
+	return total+person.budget
+},0)	
+console.log(sum)
+
+// Find
+const igor = people.find(function(person) {
+	return person.name ==='Vladilen'
+})
+console.log(igor)
+// FindIndex
+
+const igorIndex = people.findIndex(function(person) {
+	return person.name ==='Vladilen'
+})
+console.log(igorIndex)
