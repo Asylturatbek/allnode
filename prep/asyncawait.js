81eb744d25c984a5ddffc72e7f8635cf82659261
Asylturatbek <asylturatbek@gmail.com>
2020-02-16 12:11:34 +0600

    async and await functions

--- prep/asyncawait.js
+++ prep/asyncawait.js
@@ -1,4 +1,6 @@
 
+const fetch = require('node-fetch');
+console.log('Starting')
 
 const delay = ms => {
 	return new Promise(r => {
@@ -11,7 +13,37 @@ const delay = ms => {
 url = 'https://jsonplaceholder.typicode.com/todos/1'
 
 function fetchTodos() {
-	delay(2000).then(() => {
-		return fetch(url)
-	}).then(reponse => response.json())
+	console.log('Fetch to do is started')
+	return delay(2000)
+		.then(() => fetch(url))
+		.then(response => response.json())
+
 }
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => response.json())
+//   .then(json => console.log(json))
+
+// fetchTodos().then(data => {
+// 	console.log('Data: ', data)
+// })
+// .catch(e => console.error(e))
+
+async function fetchAsyncTodos() {
+	console.log('Fetch to do is started')
+	try {
+		await delay(2000)
+		const response = await fetch(url)
+		const data = await response.json()
+		console.log('Data: ', data)
+	} catch (e) {
+		console.error(e)
+	} finally {
+		return delay(1000)
+		.then(() => {
+			console.log('Fetching data is completed')
+		})
+	}
+}
+
+fetchAsyncTodos()
