f19ac57701d44da3da1e02a3ef7acb3a9b0a5d7e
Asylturatbek <asylturatbek@gmail.com>
2020-02-19 17:11:27 +0600

    added profiles schema, dummy json file and routed api get request

--- mongo-project/routes/api.js
+++ mongo-project/routes/api.js
@@ -2,26 +2,27 @@
 const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
+const Profile = require('../models/Profile.js')
 
 /*  This is a sample API route. */
 
-router.get('/:resource', (req, res) => {
-	res.json({
-		confirmation: 'success',
-		resource: req.params.resource,
-		query: req.query // from the url query string
+router.get('/profile', (req, res) => {
+	const query = req.query
+	
+	Profile.find(query)
+	.then(profiles => {
+		res.json({
+			confirmation: 'success',
+			data: profiles
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
 	})
 })
-
-router.get('/:resource/:id', (req, res) => {
-	res.json({
-		confirmation: 'success',
-		resource: req.params.resource,
-		id: req.params.id,
-		query: req.query // from the url query string
-	})
-})
-
 
 
 module.exports = router
