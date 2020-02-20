58f4cf949200a168d0193f98aeeb034208237b21
Asylturatbek <asylturatbek@gmail.com>
2020-02-20 16:40:26 +0600

    final change

--- mongo-project/routes/api.js
+++ mongo-project/routes/api.js
@@ -24,10 +24,12 @@ router.get('/profile', (req, res) => {
 	})
 })
 
-router.get('/profile/:id', (req, res) => {
-	const id = req.params.id
+router.get('/profile/update', (req, res) => {
+	const query = req.query
+	const profileId = query.id
+	delete query['id']
 
-	Profile.findById(id)
+	Profile.findByIdAndUpdate(profileId, query, {new:true})
 	.then(profile => {
 		res.json({
 			confirmation: 'success',
@@ -42,5 +44,57 @@ router.get('/profile/:id', (req, res) => {
 	})
 })
 
+router.get('/profile/remove', (req, res) => {
+	const query = req.query
+
+	Profile.findByIdAndRemove(query.id)
+	.then(data => {
+		res.json({
+			confirmation: 'success',
+			data: `Profile ${query.id} successfully removed`
+		})
+	})
+	.catch(err =>{
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+})
+
+router.get('/profile/:id', (req, res) => {
+	const id = req.params.id
+
+	Profile.findById(id)
+	.then(profile => {
+		res.json({
+			confirmation: 'success',
+			data: profile
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: `Profile id: ${id} not found`
+		})
+	})
+})
+
+router.post('/profile', (req, res) => {
+	
+	Profile.create(req.body)
+	.then(profile => {
+		res.json({
+			confirmation: 'succes',
+			data: profile
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+})
 
 module.exports = router
