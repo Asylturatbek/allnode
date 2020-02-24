cf868f505e7bef86860b400d04dfbbf4143f82ee
Asylturatbek <asylturatbek@gmail.com>
2020-02-24 22:43:15 +0600

    obrabotchik sobytiy

--- newtut/index.js
+++ newtut/index.js
@@ -1,2 +1,31 @@
-console.log(__dirname)
-console.log(__filename)
+// console.log(__dirname)
+// console.log(__filename)
+
+const events = require('events')
+const util = require('util')
+
+const myEmit = new events.EventEmitter();
+
+myEmit.on('some_event', function(text) {
+	console.log(text)
+})
+
+myEmit.emit('some_event', "obrabotchik sobytyi srabotal")
+
+const Cars = function(model) {
+	this.model = model;
+};
+
+util.inherits(Cars, events.EventEmitter)
+
+const bmw = new Cars('bmw')
+const audi = new Cars('audi')
+const volvo = new Cars('volvo')
+
+var cars = [bmw, audi, volvo]
+cars.forEach(function(car) {
+	car.on('speed', function(text) {
+		console.log(car.model + " speed is " +text)
+	})
+})
+bmw.emit('speed', '245.5 km')
